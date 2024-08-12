6c8876c2db29cb32164d8a6e47fa64cbe962127a
roian <adrianroycid@gmail.com>
2024-08-12 11:43:29 +0800

    allowing dead code

--- part1/lesson04_enums/src/main.rs
+++ part1/lesson04_enums/src/main.rs
@@ -1,5 +1,9 @@
+#![allow(dead_code)]
 mod mytypes;
 use mytypes::Color;
+
+// mod mytypes_for_demo_purposes;
+// use mytypes_for_demo_purposes::{Color, HouseLocation};
 
 fn main() {
     demo_simple_enums();
@@ -15,4 +19,10 @@ fn demo_simple_enums() {
         Color::Green => println!("gwrydd"),
         Color::Blue => println!("glas"),
     }
+
+    // let size = std::mem::size_of::<Color>();
+    // println!("{}", size);
+}
+fn main2() {
+    demo_simple_enums();
 }
