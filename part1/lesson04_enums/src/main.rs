30baad321e6b8765880aea0b2bedf063266cf15a
roian <adrianroycid@gmail.com>
2024-08-12 12:00:11 +0800

    enums with data

--- part1/lesson04_enums/src/main.rs
+++ part1/lesson04_enums/src/main.rs
@@ -1,12 +1,13 @@
-#![allow(dead_code)]
-mod mytypes;
-use mytypes::Color;
+// #![allow(dead_code)]
+// mod mytypes;
+// use mytypes::Color;
 
-// mod mytypes_for_demo_purposes;
-// use mytypes_for_demo_purposes::{Color, HouseLocation};
+mod mytypes_for_demo_purposes;
+use mytypes_for_demo_purposes::{Color, HouseLocation};
 
 fn main() {
-    demo_simple_enums();
+    // demo_simple_enums();
+    demo_with_data();
 }
 
 fn demo_simple_enums() {
@@ -23,6 +24,21 @@ fn demo_simple_enums() {
     // let size = std::mem::size_of::<Color>();
     // println!("{}", size);
 }
-fn main2() {
-    demo_simple_enums();
+
+fn demo_with_data() {
+    println!("\nDemo enums with data");
+
+    // let h = HouseLocation::Number(4);
+    // let h = HouseLocation::Name(String::from("cartref"));
+    let mut h = HouseLocation::Unknown;
+
+    h = HouseLocation::Number(9);
+    match h {
+        HouseLocation::Name(s) => println!("You live in a house named {}", s),
+        HouseLocation::Number(n) => println!("You live in a house number {}", n),
+        HouseLocation::Unknown => println!("Your house location is unknown"),
+    }
+
+    let size = std::mem::size_of::<HouseLocation>();
+    println!("Btw the size of HouseLocation is {} bytes", size);
 }
--- part1/lesson04_enums/src/mytypes.rs
+++ part1/lesson04_enums/src/mytypes.rs
@@ -3,3 +3,9 @@ pub enum Color {
     Green,
     Blue,
 }
+
+pub enum HouseLocation {
+    Number(i32),
+    Name(String),
+    Unknown,
+}
