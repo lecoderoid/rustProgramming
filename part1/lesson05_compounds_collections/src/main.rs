6d50c6b8d874ef227be72d281b5140e807fe3ceb
roian <adrianroycid@gmail.com>
2024-08-19 13:23:43 +0800

    tuples

--- part1/lesson05_compounds_collections/src/main.rs
+++ part1/lesson05_compounds_collections/src/main.rs
@@ -2,7 +2,8 @@ use std::collections::HashMap;
 
 fn main() {
     // demo_arrays();
-    demo_array_techniques();
+    // demo_array_techniques();
+    demo_tuples();
 }
 
 fn demo_arrays() {
@@ -40,4 +41,28 @@ fn demo_array_techniques() {
     a2[0] = 43;
     a2[4] = 42;
     println!("a2 is {:?}", a2);
+}
+
+fn demo_tuples() {
+    // a tuple is a fixed-sized heterogeneous collection
+    let t1 = (2, "h1", 3.42);
+    println!("t1 elements are {}, {}, {}", t1.0, t1.1, t1.2);
+
+    // you can also create a mut tuple ( you have to be consistent with element types)
+    let mut t2 = (32, "hello", 23.3);
+    t2.0 = 43;
+    println!("t2 elements are {}, {}, {}", t2.0, t2.1, t2.2);
+
+    // you can create an empty tuple (handy for functions that returns nothing)
+    let t3 = ();
+    println!("t3 is {:?}", t3);
+
+    //you can specify the type info
+    let t4: (i32, bool, bool, f64);
+    t4 = (43, true, false, 3.14);
+
+    println!(
+        "t4 is {:?}, elements are {}, {}, {}, {}",
+        t4, t4.0, t4.1, t4.2, t4.3
+    );
 }
